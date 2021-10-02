3da5a5e97afb4a0676a519e53dc7e90c69c4d7c9
Mohammad Abolnejadian <mohammad1379_a@yahoo.com>
2021-10-02 22:30:35 +0330

    the sexiest shopping cart made

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -5,8 +5,291 @@ import 'package:option_car/widgets/cart_option_card.dart';
 import 'package:option_car/widgets/persian_text.dart';
 import 'package:provider/provider.dart';
 
-class CartScreen extends StatelessWidget {
+class CartScreen extends StatefulWidget {
   const CartScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 2),
+    vsync: this,
+  )..repeat(reverse: true);
+  late final Animation<Offset> _animation = Tween<Offset>(
+    begin: Offset.zero,
+    end: const Offset(0.0, -0.5),
+  ).animate(
+    CurvedAnimation(parent: _controller, curve: Curves.decelerate),
+  );
+  late final Animation<Offset> _animationTwo = Tween<Offset>(
+    begin: Offset.zero,
+    end: const Offset(0.0, 0.5),
+  ).animate(
+    CurvedAnimation(parent: _controller, curve: Curves.decelerate),
+  );
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Widget pageOne(MediaQueryData _mediaQuery, OptionProvider _provider) =>
+      Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(right: _mediaQuery.size.width * 0.1),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                PersianText(
+                  "نصب",
+                  style: TextStyle(
+                    fontSize: _mediaQuery.textScaleFactor * 20,
+                    color: Color(0xff707070),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                SvgPicture.asset(
+                  "assets/images/icons/inPlace_Install_Button.svg",
+                  height: 25,
+                  width: 25,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemBuilder: (_, i) {
+                if (i == 0) {
+                  return Padding(
+                    padding: const EdgeInsets.only(
+                      top: 10,
+                    ),
+                    child: CartOptionCard(
+                      option: _provider.options[i],
+                    ),
+                  );
+                }
+                if (i == _provider.options.length - 1) {
+                  return Padding(
+                    padding: const EdgeInsets.only(
+                      bottom: 10,
+                    ),
+                    child: CartOptionCard(
+                      option: _provider.options[i],
+                    ),
+                  );
+                }
+                return CartOptionCard(
+                  option: _provider.options[i],
+                );
+              },
+              itemCount: _provider.options.length,
+            ),
+          ),
+          Container(
+            height: _mediaQuery.size.height * 0.1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.chevron_left,
+                        color: Color(0xff00f59c),
+                        size: 30,
+                      ),
+                      PersianText(
+                        "تعیین زمان نصب",
+                        style: TextStyle(
+                          color: Color(0xff00f59c),
+                          fontSize: _mediaQuery.textScaleFactor * 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Spacer(flex: 1),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SlideTransition(
+                      child: Icon(Icons.keyboard_arrow_up),
+                      position: _animation,
+                    ),
+                    PersianText(
+                      "به بالا بکشید.",
+                      style: TextStyle(
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                  ],
+                ),
+                Spacer(flex: 3),
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: Row(
+                    children: [
+                      PersianText(
+                        "تحویل",
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(width: 5),
+                      SvgPicture.asset(
+                        "assets/images/icons/delivering.svg",
+                        color: Colors.grey,
+                        width: 15,
+                        height: 15,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      );
+
+  Widget pageTwo(
+    MediaQueryData _mediaQuery,
+    OptionProvider _provider,
+  ) =>
+      Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                PersianText(
+                  "نصب",
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(width: 5),
+                SvgPicture.asset(
+                  "assets/images/icons/inPlace_Install_Button.svg",
+                  color: Colors.grey,
+                  width: 15,
+                  height: 15,
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: _mediaQuery.size.width * 0.1),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(),
+                Spacer(flex: 4),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    PersianText(
+                      "به پایین بکشید.",
+                      style: TextStyle(
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                    SlideTransition(
+                      child: Icon(Icons.keyboard_arrow_down),
+                      position: _animationTwo,
+                    ),
+                  ],
+                ),
+                Spacer(flex: 1),
+                Row(
+                  children: [
+                    PersianText(
+                      "تحویل",
+                      style: TextStyle(
+                        fontSize: _mediaQuery.textScaleFactor * 20,
+                        color: Color(0xff707070),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    SvgPicture.asset(
+                      "assets/images/icons/delivering.svg",
+                      height: 25,
+                      width: 25,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemBuilder: (_, i) {
+                if (i == 0) {
+                  return Padding(
+                    padding: const EdgeInsets.only(
+                      top: 10,
+                    ),
+                    child: CartOptionCard(
+                      option: _provider.options[i],
+                    ),
+                  );
+                }
+                if (i == _provider.options.length - 1) {
+                  return Padding(
+                    padding: const EdgeInsets.only(
+                      bottom: 10,
+                    ),
+                    child: CartOptionCard(
+                      option: _provider.options[i],
+                    ),
+                  );
+                }
+                return CartOptionCard(
+                  option: _provider.options[i],
+                );
+              },
+              itemCount: _provider.options.length,
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Row(
+              children: [
+                Icon(
+                  Icons.chevron_left,
+                  color: Color(0xff00f59c),
+                  size: 30,
+                ),
+                PersianText(
+                  "تعیین زمان تحویل",
+                  style: TextStyle(
+                    color: Color(0xff00f59c),
+                    fontSize: _mediaQuery.textScaleFactor * 15,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      );
 
   @override
   Widget build(BuildContext context) {
@@ -83,93 +366,22 @@ class CartScreen extends StatelessWidget {
                 ],
               ),
             ),
-            // Padding(
-            //   padding: EdgeInsets.only(
-            //     right: _mediaQuery.size.width * 0.05,
-            //   ),
-            //   child: Row(
-            //     mainAxisAlignment: MainAxisAlignment.end,
-            //     children: [
-            //       PersianText(
-            //         "سبد خرید",
-            //         style: TextStyle(
-            //           fontSize: _mediaQuery.textScaleFactor * 20,
-            //         ),
-            //       ),
-            //       SizedBox(
-            //         width: _mediaQuery.size.width * 0.1,
-            //       ),
-            //       Icon(
-            //         Icons.shopping_cart,
-            //         color: Color(0xff707070),
-            //         size: 30,
-            //       ),
-            //     ],
-            //   ),
-            // ),
             SizedBox(
               height: _mediaQuery.size.height * 0.01,
             ),
-            Padding(
-              padding: EdgeInsets.only(right: _mediaQuery.size.width * 0.1),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
+            Expanded(
+              child: PageView(
+                scrollDirection: Axis.vertical,
+                controller: PageController(
+                  initialPage: 0,
+                ),
+                physics: PageScrollPhysics(),
                 children: [
-                  PersianText(
-                    "نصب",
-                    style: TextStyle(
-                      fontSize: _mediaQuery.textScaleFactor * 20,
-                      color: Color(0xff707070),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  SvgPicture.asset(
-                    "assets/images/icons/inPlace_Install_Button.svg",
-                    height: 25,
-                    width: 25,
-                  ),
+                  pageOne(_mediaQuery, _provider),
+                  pageTwo(_mediaQuery, _provider),
                 ],
               ),
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            Container(
-              height: _mediaQuery.size.height * 0.35,
-              child: ListView.builder(
-                // shrinkWrap: true,
-                // semanticChildCount: 3,
-                // physics: FixedExtentScrollPhysics(),
-                itemBuilder: (_, i) {
-                  if (i == 0) {
-                    return Padding(
-                      padding: const EdgeInsets.only(
-                        top: 10,
-                      ),
-                      child: CartOptionCard(
-                        option: _provider.options[i],
-                      ),
-                    );
-                  }
-                  if (i == _provider.options.length - 1) {
-                    return Padding(
-                      padding: const EdgeInsets.only(
-                        bottom: 10,
-                      ),
-                      child: CartOptionCard(
-                        option: _provider.options[i],
-                      ),
-                    );
-                  }
-                  return CartOptionCard(
-                    option: _provider.options[i],
-                  );
-                },
-                itemCount: _provider.options.length,
-              ),
-            ),
+            )
           ],
         ),
       ),
